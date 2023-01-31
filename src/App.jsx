492e2b33ef8baba546ac07b15aa0cf126a2e3826
Mardonio-Abreu <mardonio.abreu@protonmail.com>
2023-01-30 21:44:59 -0600

    Primera entrega (corrección)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './css/style.css';
 import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
   <>
   
     <NavBar />
+    <ItemListContainer />
     
   </>
   
---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,13 @@
+function ItemListContainer (){
+
+return (
+
+<>
+    <h3>"2600 Merchandise"</h3>
+</>
+
+);
+
+}
+
+export default ItemListContainer;
