d1d9631e4646595f2b65754e8413452b22aa0a96
Mardonio-Abreu <mardonio.abreu@protonmail.com>
2023-04-21 19:15:31 -0600

    Firebase query --first debug--

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,14 +1,36 @@
+import { useEffect, useState } from "react";
+import { doc, getFirestore, getDocs, collection } from "firebase/firestore";
 import items from "../../products/data";
 import BasicCard from "../Card/Card";
 
 function ItemListContainer() {
-  let ItemList = [];
+  const [itemsCollection, setProducts] = useState();
+  useEffect(() => {
+    const db = getFirestore();
 
-  for (let i = 0; i < items.length; i++) {
+    const items = collection(db, "items");
+
+    getDocs(itemsCollection).then((snapshot) => {
+      setProducts(
+        snapshot.docs.map((doc) => ({
+          id: snapshot.item_id,
+          item: snapshot.item,
+          description: snapshot.description,
+          image: snapshot.image,
+          price: snapshot.price,
+          data: snapshot.data(),
+        }))
+      );
+    });
+  }, []);
+
+  // let ItemList = [];
+
+  /* for (let i = 0; i < items.length; i++) {
     ItemList[i] = items[i].map((element) => <BasicCard props={element} />);
-  }
+  } */
 
-  return ItemList;
+  //return ItemList;
 }
 
 export default ItemListContainer;
