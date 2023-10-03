a68e41dc59a90680eb816fc456dfa158443f22fc
Mardonio-Abreu <mardonio.abreu@protonmail.com>
2023-10-03 17:45:38 -0600

    context bugs added

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,7 +1,10 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import { useContext } from "react";
 
 function ItemDetail() {
+  const itemValues = useContext();
+
   return (
     <Card style={{ width: "18rem" }}>
       <Card.Img variant="top" src="holder.js/100px180" />
