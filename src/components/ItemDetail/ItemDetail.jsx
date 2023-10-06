9ce3f1e07c10b8982a9437b936125a1b2f3b53b5
Mardonio-Abreu <mardonio.abreu@protonmail.com>
2023-10-06 11:17:01 -0600

    Itemdetail bug fixed

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -4,39 +4,37 @@ import { db } from "../../firebase/firebaseConfig";
 import { doc, getDoc } from "firebase/firestore";
 
 export function ItemDetail() {
-  let { itemId } = useParams();
-  const [item, setItem] = useState([]);
-  const docRef = doc(db, "items", 1);
+  const { id } = useParams();
+  const [item, setItem] = useState(null);
+  const docRef = doc(db, "items", id);
 
   useEffect(() => {
     const getItems = async () => {
       const docSnap = await getDoc(docRef);
       if (docSnap.exists()) {
         console.log("Document data:", docSnap.data());
-        setItem(docSnap.data());
-        console.log(docSnap.data);
       } else {
         // docSnap.data() will be undefined in this case
         console.log("No such document!");
       }
-      setItem(docSnap);
+      setItem(docSnap.data());
     };
 
     getItems();
     console.log(item);
-  }, [item, itemId, docRef]);
+  }, []);
 
   return (
     <div>
       {item ? (
         <>
           <h1>Item Detail</h1>
-          <p>Item Name: {item.item}</p>
+          <p>Item Name: {item.price}</p>
           <p>Item Description: {item.description}</p>
           {/* Agrega más campos aquí según tu estructura de datos */}
         </>
       ) : (
-        <p>Loading...</p>
+        <p>The item doesn't exist...</p>
       )}
     </div>
   );
